9c0271b84683f8a1a13c493882e774e6d87fcbee
Yolina Petkova <yolina.petkova@strypes.eu>
2023-04-05 12:07:53 +0300

    -Added average and within range.

--- TestClass.cpp
+++ TestClass.cpp
@@ -20,6 +20,7 @@ class TestClass
         int var2;
         double var3;
         double var4;
+        int range = 10;
 
         TestClass(int a, int b)
         {
@@ -38,27 +39,56 @@ class TestClass
             return (var1 + var2);
         }
 
-        double getSum(double var1, double var2)
+        int getDifference(void)
         {
-            return (var3 + var4);
+            return (var1 - var2);
         }
 
-        void getQuotientAndRemainer(int *quotient, int *remainer)
+        int getProduct(void)
+        {
+            return (var1 * var2);
+        }
+
+        // double getSum(double var1, double var2)
+        // {
+        //     return (var3 + var4);
+        // }
+
+        void getQuotientAndRemainder(int *quotient, int *remainer)
         {
             if ((quotient == NULL) && (remainer == NULL))
             {
                 print_log(FILE, LINE, "NULL ptr");
             }
+
+            if (var2 == 0)
+            {
+                print_log(FILE, LINE, "Error: cannot divide by zero.");
+            }
+
             *quotient = var1 / var2;
             *remainer = var1 % var2;
+        } 
+
+        int getAverage(void)
+        {
+            return ((var1 + var2)/2);
         }
 
-        // int sum;
-        // int product;
-        // int difference;
-        // int quotient;
-        // int remainder;
-        // double 
+        bool isAverageWithinDesiredRange(void)
+        {
+            if (getAverage() > range)
+            {
+                // printf("ERROR: Average outside of range. [range=%d]", range);
+                return false;
+            }
+            return true;
+        }
+
+        int getRangeValue(void)
+        {
+            return range;
+        }
 };
 
 
@@ -72,25 +102,59 @@ int main() {
     // printf("F:%s L:%d Test print\n", FILE, LINE);
     print_log(FILE, LINE, "Test print");
 
-    int a = 8;
-    int b = 5;
+    // int a = 8;
+    // int b = 5;
+
+    int a;
+    int b;
+
+    cout << "Enter first integer value: ";
+    cin >> a;
+    cout << "Enter second integer value: ";
+    cin >> b;
+
+    printf("Working with integers: %d and %d\n", a, b);
+
+    //Add error checking...
 
     TestClass test1(a, b);
-    printf("%d\n", test1.getSum());
+    printf("Sum: %d\n", test1.getSum());
 
+    printf("Difference: %d\n", test1.getDifference());
+
+    printf("Product: %d\n", test1.getProduct());
+
+#if 0
     double x = 8.0;
     double y = 5.5;
 
     TestClass test2(x, y);
     printf("%f\n", test2.getSum(x, y));
+#endif
 
     int quotient;
     int remainder;
-    test1.getQuotientAndRemainer(&quotient, &remainder);
+    test1.getQuotientAndRemainder(&quotient, &remainder);
 
+    //QN: HOW DO I MAKE A FUNCTION WITH VARIABLE NUMBER OF PARAMS? CAN THAT BE DONE? OR IS MACRO THE BEST WAY TO GO HERE?
     char message[LOG_LENGTH] = {0};
-    snprintf(message, LOG_LENGTH, "quotient=%d, remainer=%d", quotient, remainder);
+    snprintf(message, LOG_LENGTH, "quotient=%d, remainder=%d", quotient, remainder);
     print_log(FILE, LINE, message);
+
+    printf("Average: %d\n", test1.getAverage());
+
+    
+
+    // bool is_within_range = test1.isAverageWithinDesiredRange();
+
+    // if (is_within_range == true)
+    // {
+    // }
+
+    printf("Average is %s range. [range=%d]\n", 
+            (test1.isAverageWithinDesiredRange() == true) ? "WITHIN" : "OUTSIDE OF",
+            test1.getRangeValue());
+    
 
     return 0;
 }
