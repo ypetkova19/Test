062c4de3b51c9cc4861b203d0fadaa548abacd72
Yolina Petkova <yolina.petkova@strypes.eu>
2023-04-05 16:35:23 +0300

    -Added divided value method

--- TestClass.cpp
+++ TestClass.cpp
@@ -22,8 +22,10 @@ class TestClass
     private:
         int var1;
         int var2;
-        double var3;
-        double var4;
+        // double var3;
+        // double var4;
+        int max_sum_value;
+        int max_diff_value;
         int max_average_value;
         int max_square_root_value;
 
@@ -34,25 +36,49 @@ class TestClass
         {
             var1 = a;
             var2 = b;
+            max_sum_value = 16;
+            max_diff_value = 5;
             max_average_value = 10;
             max_square_root_value = 7;
         }
 
-        //do not create constructors with same number of parameters, ideally different number of parameters
+#if 0
+        // do not create constructors with same number of parameters, ideally different number of parameters
         TestClass(double a, double b)
         {
             var3 = a;
             var4 = b;
         }
 
-        // double getSum(double var1, double var2)
-        // {
-        //     return (var3 + var4);
-        // }
+        double getSum(double var1, double var2)
+        {
+            return (var3 + var4);
+        }
+#endif        
 
         int getSum(void)
         {
             return (var1 + var2);
+        }
+
+        int getAverage(void)
+        {
+            return ((var1 + var2)/2);
+        }
+
+        int getMaxAverageValue(void)
+        {
+            return max_average_value;
+        }
+
+        bool isAverageWithinDesiredMaxValue(void) //remove void
+        {
+            if (getAverage() > getMaxAverageValue())
+            {
+                // printf("ERROR: Average outside of max_average_value. [max_average_value=%d]", max_average_value);
+                return false;
+            }
+            return true;
         }
 
         int getDifference(void)
@@ -70,58 +96,76 @@ class TestClass
             return sqrt(getProduct());
         }
 
+        int getMaxSquareRootValue(void)
+        {
+            return max_square_root_value;
+        }
+
         bool isSquareRootOfProductWithinDesiredMaxValue(void)
         {
-            if (getSquareRoot() > max_square_root_value)
+            if (getSquareRoot() > getMaxSquareRootValue())
             {
                 return false;
             }
             return true;
         }
 
-        // void getQuotientAndRemainder(int *quotient, int *remainer)
-        void getQuotientAndRemainder(int &quotient, int &remainer)
+#if 0 //pass by pointer
+        void getQuotientAndRemainder(int *quotient, int *remainer)
         {
-            // if ((quotient == NULL) && (remainer == NULL))
-            // {
-            //     print_log(FILE, LINE, "NULL ptr");
-            // }
+            if ((quotient == NULL) && (remainer == NULL))
+            {
+                print_log(FILE, LINE, "NULL ptr");
+            }
 
             if (var2 == 0)
             {
                 print_log(FILE, LINE, "Error: cannot divide by zero.");
             }
 
-            // *quotient = var1 / var2;
-            // *remainer = var1 % var2;
+            *quotient = var1 / var2;
+            *remainer = var1 % var2;
+        } 
+#endif        
+
+        /* pass by reference */
+        void getQuotientAndRemainder(int &quotient, int &remainer)
+        {
+            try 
+            {
+                if (var2 == 0)
+                {
+                    throw(var2);
+                }
+            }
+            catch (int var2)
+            {
+                print_log(FILE, LINE, "Error: cannot divide by zero.");
+            }
 
             quotient = var1 / var2;
             remainer = var1 % var2;
         } 
 
-        int getAverage(void)
+        double getDividedValue(void)
         {
-            return ((var1 + var2)/2);
-        }
-
-        bool isAverageWithinDesiredMaxValue(void) //remove void
-        {
-            if (getAverage() > max_average_value)
+            try 
             {
-                // printf("ERROR: Average outside of max_average_value. [max_average_value=%d]", max_average_value);
-                return false;
+                if (var2 == 0)
+                {
+                    throw(var2);
+                }
             }
-            return true;
-        }
+            catch (int var2)
+            {
+                print_log(FILE, LINE, "Error: cannot divide by zero.");
+            }
 
-        int getMaxRangeValue(void)
-        {
-            return max_average_value;
-        }
+            // double tmp_var1 = var1 * 1.0;
+            // double tmp_var2 = var2 * 1.0;
 
-        int getMaxSquareRootValue(void)
-        {
-            return max_square_root_value;
+            // return (tmp_var1 / tmp_var2);
+            return (var1  * 1.0 / var2);
         }
 };
 
@@ -147,8 +191,8 @@ int main()
 
     printf("Working with integers: %d and %d\n", a, b);
 
-    //Add error checking to user input... as we cannot divide by zero.
-    assert(b != 0);
+    //Add error checking to user input... as we cannot divide by zero. MOVED CHECK ELSEWHERE.
+    // assert(b != 0);
 
     TestClass test1(a, b);
     printf("Sum: %d\n", test1.getSum());
@@ -162,15 +206,19 @@ int main()
     // test1.getQuotientAndRemainder(&quotient, &remainder);
     test1.getQuotientAndRemainder(quotient, remainder);
 
-    char message[LOG_LENGTH] = {0};
-    snprintf(message, LOG_LENGTH, "quotient=%d, remainder=%d", quotient, remainder);
-    print_log(FILE, LINE, message);
+    // char message[LOG_LENGTH] = {0};
+    // snprintf(message, LOG_LENGTH, "quotient=%d, remainder=%d", quotient, remainder);
+    // print_log(FILE, LINE, message);
+
+    printf("Quotient=%d, Remainder=%d\n", quotient, remainder);
+
+    printf("Divided value: %f\n", test1.getDividedValue());
 
     printf("Average: %d\n", test1.getAverage());
 
     printf("Average is %s max_average_value. [max_average_value=%d]\n", 
             (test1.isAverageWithinDesiredMaxValue() == true) ? "WITHIN" : "OUTSIDE OF",
-            test1.getMaxRangeValue());
+            test1.getMaxAverageValue());
 
     printf("Square root: %d\n", test1.getSquareRoot());
 
